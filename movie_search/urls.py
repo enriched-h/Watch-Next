43c23d38dbc08f23ddf85b4580d8209a2da155c8
enriched-h <30749938+enriched-h@users.noreply.github.com>
2023-08-10 12:50:02 +0200

    Update urls.py

--- movie_search/urls.py
+++ movie_search/urls.py
@@ -6,5 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('http://web-production-8a28.up.railway.app/', include('watch_next.urls')),
+    path('', include('watch_next.urls')),
 ]
